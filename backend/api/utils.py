2b93fea905487f27d31f50c555ee96bfdcab1eba
YELIZAVETA GANETSKA <eganecka@gmail.com>
2023-08-31 21:53:25 +0200

    api check

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 
 
 def download_file_response(ingredients_list):
-    buy_list = []
-    buy_list.append('Чтобы приготовить все блюда из корзины необходимо:\n')
+    buy_list = ['Чтобы приготовить все блюда из корзины необходимо:\n']
     for item in ingredients_list:
         buy_list.append(f'{item["ingredient__name"]} - {item["amount"]} '
                         f'{item["ingredient__measurement_unit"]} \n')
