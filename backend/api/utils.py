db5166da59d64f44c046ae8d871e54db776cb59e
YELIZAVETA GANETSKA <eganecka@gmail.com>
2023-09-02 21:55:47 +0200

    macbook

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,9 +5,10 @@ def download_file_response(ingredients_list):
     buy_list = ['Чтобы приготовить все блюда из корзины необходимо:\n']
     for item in ingredients_list:
         buy_list.append(f'{item["ingredient__name"]} - {item["amount"]} '
-                        f'{item["ingredient__measurement_unit"]} \n')
+                        f'{item["ingredient__measurement_unit"]}\n')
 
-    response = HttpResponse(buy_list, 'Content-Type: text/plain')
-    response['Content-Disposition'] = ('attachment; '
-                                       'filename="buylist.txt"')
+    response_content = ''.join(buy_list)
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="buylist.txt"'
+    response.write(response_content)
     return response
